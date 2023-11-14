48b3a0dbc582e58d5989597c60c00dcc027a668f
nihar <nihar@marolix.com>
2023-11-14 15:39:52 +0530

    minimal database created

--- tmFunctionalities/models.py
+++ tmFunctionalities/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+    
+class Boards(models.Model):
+    boardName = models.CharField(max_length=20)
+    description = models.TextField(max_length=100000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class List(models.Model):
+    listTitle = models.CharField(max_length=200)
+    board = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Card(models.Model):
+    cardTitle = models.CharField(max_length=200)
+    cardDescription = models.TextField()
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    board = models.ForeignKey(User, on_delete=models.CASCADE)
+    
